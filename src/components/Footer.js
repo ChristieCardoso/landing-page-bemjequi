288a17abb1f9b5541f0fc964cb284d208b7c4c1d
Christie Cardoso <christiecardoso@hotmail.com>
2023-06-19 13:32:40 -0300

    add button facebook, in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Logo from '../assets/Logo.svg'
 import { IoLogoWhatsapp } from 'react-icons/io'
 import { TiSocialInstagram } from 'react-icons/ti'
-/* import { FaFacebookF } from "react-icons/fa"; */
+import { FaFacebookF } from "react-icons/fa"
 const Footer = () => {
   const handleWhatsAppClick = () => window.open('https://api.whatsapp.com/send?phone=31993034536', '_blank');
   const handleInstagramClick = () => window.open(`https://www.instagram.com/bemjequi`, '_blank');
-  
+  const handleFcebookClick = () => window.open(`https://www.facebook.com/bemjequi`, '_blank');
+
   return (
     <div className="footer-wrapper" id="footer">
       <div className="footer-section-one">
@@ -16,6 +17,7 @@ const Footer = () => {
         <div className="footer-icons">
           <IoLogoWhatsapp onClick={handleWhatsAppClick} />
           <TiSocialInstagram onClick={handleInstagramClick} />
+          <FaFacebookF onClick={handleFcebookClick} />
         </div>
       </div>
       <div className="footer-section-two">
