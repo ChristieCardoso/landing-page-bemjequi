7ab76a63e9581023b6cb46786f9c481d8f84ef6e
Christie Cardoso <christiecardoso@hotmail.com>
2023-06-27 11:47:58 -0300

    edit about css

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,15 +3,7 @@ import AboutBackground from '../../assets/about-background.png'
 import AboutBackgroundImage from '../../assets/about-background-image.png'
 import './style.css'
 
-const About = () => {  
-  document.querySelector('a[href^="#sobre"]').addEventListener('click', function (e) {
-    e.preventDefault();
-  
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-  
+const About = () => {
   return (
     <div className="about-section-container" id="sobre">
       <div className="about-background-image">
